733be5a1e35ec650a8f7d86d4acbec8597c8c692
Purpel Lizrd <59480394+ihavefoxdie@users.noreply.github.com>
2022-09-16 20:45:54 +0300

    Update Teenager.cs

--- CHSARPCLASS/Classes/Teenager.cs
+++ CHSARPCLASS/Classes/Teenager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,29 +8,53 @@ namespace CHSARPCLASS.Classes
 {
     public class Teenager : Human
     {
-        public string School { get; private set; }
+        private string _school;
+        public string School {
+            get
+            {
+                return _school;
+            }
+            set
+            {
+                _school = value;
+            }
+        }
+
+        public new int Age
+        {
+            get
+            {
+                return _age;
+            }
+            private set
+            {
+                AgeRestrictor(value);
+                _age = value;
+            }
+        }
 
         public Teenager(string name, int age, string schoolName) : base(name, age)
         {
             AgeRestrictor(age);
             School = schoolName;
         }
-        public new void AgeSet(int age)
+
+        public void SetAge (int age)
         {
             AgeRestrictor(age);
-            Age = age;
+            _age = age;
         }
 
         private void AgeRestrictor(int age)
         {
-            if (!(age >= 13) && !(age <= 19))
+            if (!((age >= 13) && (age <= 19)))
                 throw new Exception("Invalid age!");
         }
 
-        public void SchoolSet(string schoolName)
+        /*public void SchoolSet(string schoolName)
         {
             School = schoolName;
-        }
+        }*/
 
         public new string Print()
         {
