97011e349d7bf16662aa44c13e52cd4e66dcb6a9
Purpel Lizrd <59480394+ihavefoxdie@users.noreply.github.com>
2022-09-16 20:44:54 +0300

    Update Program.cs

--- CHSARPCLASS/Program.cs
+++ CHSARPCLASS/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using CHSARPCLASS.Classes;
 
 
@@ -10,15 +10,15 @@ namespace Testing
         {
             var Kenny = new Human("Kenny", 20);
             Console.WriteLine(Kenny.Print());
-            Kenny.AgeSet(20);
-            Kenny.NameSet("Kenny");
+            Kenny.Age = 20;
+            Kenny.Name = "Kenny";
             Console.WriteLine(Kenny.Print());
 
             var Kid = new Teenager("Paul", 14, "School 14");
             Console.WriteLine(Kid.Print());
-            Kid.NameSet("Lisa");
-            Kid.AgeSet(13);
-            Kid.SchoolSet("School 15");
+            Kid.Name = "Lisa";
+            Kid.SetAge(10);
+            Kid.School = "School 19";
             Console.WriteLine(Kid.Print());
         }
     }
